f250f1a7baa3285d9cb9880322fa2f286c15b086
Chris Smith <chrisroadmap@gmail.com>
2025-06-23 22:14:29 +0200

    update legend

--- notebooks/10_volcanic-forcing.py
+++ notebooks/10_volcanic-forcing.py
@@ -5,7 +5,7 @@
 #       extension: .py
 #       format_name: percent
 #       format_version: '1.3'
-#       jupytext_version: 1.16.6
+#       jupytext_version: 1.17.0
 #   kernelspec:
 #     display_name: Python 3 (ipykernel)
 #     language: python
@@ -218,8 +218,8 @@ pl.legend()
 
 # %%
 pl.plot(np.arange(1979+1/24,2024+1/24,1/12), aod_glossac, label='GloSSAC v2.2.2 (1979-2023)', lw=0.5)
-pl.plot(np.arange(1975+1/24,2024+1/24,1/12), aod_cmip7[-588:], label='CMIP6 (1850-2023)', lw=0.5)
-pl.plot(np.arange(2013+1/24,2025+1/24,1/12), aod_omps, label='OMPS (2013-2024)', lw=0.5)
+pl.plot(np.arange(1975+1/24,2024+1/24,1/12), aod_cmip7[-588:], label='CMIP7 v1.3.0 (1850-2023)', lw=0.5)
+pl.plot(np.arange(2013+1/24,2025+1/24,1/12), aod_omps, label='OMPS LP (2013-2024)', lw=0.5)
 pl.plot(np.arange(1975+1/24,2025+1/24,1/12), aod[-600:], label='Combined', zorder=-2, color='k')
 pl.title('Stratospheric aerosol optical depth')
 pl.ylabel("AOD")
